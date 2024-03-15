9d7bdeeec897f871dce9025a6bdc4a6134042857
Marlon Olimpio <58260665+Olimpio-Marlon@users.noreply.github.com>
2024-03-15 14:14:05 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO
+{
+    internal class Calculadora
+    {
+        public void Somar(double x, double y)
+        {
+            Console.WriteLine($" {x} + {y} = { x + y}");
+        }
+        public void Subtrair(double x, double y)
+        {
+            Console.WriteLine($" {x} - {y} = {x - y}");
+        }
+        public void Multiplicar(double x, double y)
+        {
+            Console.WriteLine($" {x} X {y} = {x * y}");
+        }
+        public void Dividir(double x, double y)
+        {
+            Console.WriteLine($" {x} / {y} = {x / y}");
+        }
+        public void Resto(double x, double y)
+        {
+            Console.WriteLine($" {x} Resto {y} = {x % y}");
+        }
+        
+        // Para fazer o cáuculo da potência e o cáuculo da raiz é necessário usar o Math.
+        public void Potencia(double x, double y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($" {x}^{y} = {pot}");
+        }
+        
+        public void Raiz(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine("O valor da raiz eh " + raiz);
+        }
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using DIO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Linq;
+using System.Collections.Generic;
+
+Calculadora calc = new Calculadora();
+
+
+Console.Write($"Digite o 1º valor: ");
+double valor1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Digite o 2º valor: ");
+double valor2 = Convert.ToInt32(Console.ReadLine());
+
+
+calc.Potencia(valor1,valor2);
+
+calc.Somar(valor1, valor2);
+
+calc.Subtrair(valor1, valor2);
+
+calc.Multiplicar(valor1, valor2);
+
+calc.Dividir(valor1, valor2);
+
+calc.Resto(valor1, valor2);
+
+
+
+Console.ReadKey();
